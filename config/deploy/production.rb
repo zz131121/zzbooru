42f4e56a9aa1d722aaf3e71a474abfd8aa2988f6
root <root@vultr.guest>
2017-04-18 07:31:23 +0000

    zz131121

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-set :user, "albert"
+set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "45.32.82.11", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "45.32.82.11", :roles => %w(web app), :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
